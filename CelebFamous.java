05f556faf0e39f8acb209821d565ef93e8563307
Abas24 <asheikh3@email.cpcc.edu>
2016-04-24 01:37:50 -0400

    Main Method
    Get user information

---
+++ CelebFamous.java
@@ -0,0 +1,26 @@
+
+
+public class CelebFamous
+{
+
+
+public static void main(String[] args)
+    {
+    
+    
+      System.out.println("Enter your birthday");
+      
+
+
+
+
+
+
+     }
+
+
+
+
+
+
+}
